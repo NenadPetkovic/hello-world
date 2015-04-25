9374dc54f45d53a6bdfff37032d08a0be5e3838f
Nenad Home <nenad.ppetkovic@gmail.com>
2015-04-25 02:19:49 +0200

    Add state machine implementation for incremental changing program and incremental volume change.

--- main.c
+++ main.c
@@ -4,7 +4,16 @@
 #include <stdlib.h>
 
 
-#define BUFFER_SIZE 3
+#define BUFFER_SIZE 20
+#define BIG_PAUSE_COUNT 1400
+#define SMALL_PAUSE_COUNT 380
+#define LOOP_PAUSE 1400
+
+typedef enum{inital, bigPause, smallPause, upDownVolume} tStateVolume;
+typedef enum{initalPrg, loop} tStateProgram; 
+
+tStateVolume volumeState=inital;
+tStateProgram programState= initalPrg;
 
 int buffer[BUFFER_SIZE];
 int start=0, end=0;
@@ -12,8 +21,60 @@ pthread_mutex_t mutex, printMutex;
 pthread_cond_t full_cv, empty_cv;
 
 
+int bigPauseCount=0;
+int smallPauseCount=0;
+void volumeStateMachine(int i){
+	//if(!i){volumeState=inital; return;}
+	switch(volumeState){
+		case inital:
+			produceCommand(i);
+			volumeState=bigPause;
+			bigPauseCount=0;
+			smallPauseCount=0;
+			break;
+		case bigPause:
+			if(bigPauseCount==BIG_PAUSE_COUNT){
+				produceCommand(i);
+				volumeState=smallPause;
+			}else{
+				bigPauseCount++;
+				//reamin in the same state//volumeState=bigPause;
+			}
+			break;
+		case smallPause:
+			if(smallPauseCount==SMALL_PAUSE_COUNT){
+				//volumeState=upDownVolume;
+				produceCommand(i);
+				smallPauseCount=0;
+			}else{
+				smallPauseCount++;
+				//reamin in same state
+			}
+			break;
+		case upDownVolume:
+			break;
+	}
 
+}
 
+int loop_pause=0;
+void programStateMachine(int i){
+	switch(programState){
+		case initalPrg:
+			loop_pause=0;
+			programState=loop;
+			break;
+		case loop:
+			if(loop_pause==LOOP_PAUSE){
+				produceCommand(i);
+				loop_pause=0;
+			}else{
+				loop_pause++;
+			}
+			break;
+	}
+
+}
 int consumeCommand(){
 	int command;
 	pthread_mutex_lock(&mutex);
@@ -44,16 +105,20 @@ void produceCommand(int command){
 void producerThread(void* t){
 	long id=(long)t;
 	srand(time(NULL));
-	int i=0;
+	int i=1;
 	int command;
-	while(i<10){
-		command = rand()%1000;
-		produceCommand(command);
+	while(1){
+		command = rand()%10;
+		//volumeStateMachine(command);
+		programStateMachine(command);
 		pthread_mutex_lock(&printMutex);
-		printf("[Producer]:	with id %d produced command	%d \n", id, command);
+		//printf("[Producer]:	with id %d produced command	%d \n", id, command);
 		pthread_mutex_unlock(&printMutex);
 		i++;
-		sleep(1);
+		usleep(200);
+		/*pthread_mutex_lock(&printMutex);
+		printf("[Producer]");
+		pthread_mutex_unlock(&printMutex);*/
 	}
 
 }
@@ -61,14 +126,17 @@ void producerThread(void* t){
 
 void consumerThread(void* t){
 	long id=(long)t;
-	int command, i=0;
-	while(i<10){
+	int command, i=1;
+	while(1){
 		command = consumeCommand();
 		pthread_mutex_lock(&printMutex);
 		printf("[Consumer]:	with id %d consumed command	%d \n", id, command);
 		pthread_mutex_unlock(&printMutex);
 		i++;
-		sleep(3);
+		//usleep(1200);
+		/*pthread_mutex_lock(&printMutex);
+		printf("[Producer]");
+		pthread_mutex_unlock(&printMutex);*/
 	}
 }
 
